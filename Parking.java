29db5174af6f594024d1e311a343d119ab24c3f5
dipenpratap <47872784+dipenpratap@users.noreply.github.com>
2020-02-11 23:43:46 -0600

    Add files via upload

---
+++ Parking.java
@@ -0,0 +1,90 @@
+// This class asks the user for type of vehicle and calculates the parking fare on the basis of:
+// 1. Type of vehicle
+// 2. Total hours parked
+
+// The rate of parking is as follows:
+/*
+
+TWO WHEELER : $0.00/hr first 3 hr $1.50/hr after 3 hr
+CAR 		: $1.00/hr first 2 hr $2.30/hr after 2 hr
+BUS 		: $2.00/hr first hr $3.70/hr after 1 hr
+
+- Dipendra Yadav
+
+*/
+
+
+import java.util.*;
+import java.text.*;
+
+class Parking {
+	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("$##.##");
+		Scanner input = new Scanner(System.in);
+		double hr = 0.0;
+		int menu = 0;
+		System.out.println("PARKING CHARGES");
+		System.out.println();
+		System.out.println("1. TWO WHEELER");
+		System.out.println("2. CAR");
+		System.out.println("3. TRUCK");
+		System.out.println("4. EXIT");
+		boolean quit = false;
+		do {
+			System.out.print("PLEASE ENTER YOUR CHOICE: ");
+			menu = input.nextInt();
+			System.out.println();
+
+			switch (menu) {
+			case 1:
+				System.out.print("ENTER THE TOTAL HOURS: ");
+				hr = input.nextDouble();
+
+				System.out.println("============ CHARGES ===============");
+				System.out.println("VEHICLE TYPE: TWO WHEELER");
+				if (hr > 2) {                   // NO CHARGE FOR FIRST TWO HOURS
+					double amount = (hr - 2) * 2.00;   // $2/HR AFTERWARDS
+					System.out.println("CHARGES = " + df.format(amount));
+				} else {
+					System.out.println("NO CHARGE! FIRST 2 MINUTES IS FREE");
+				}
+				break;
+			case 2:
+				System.out.print("ENTER THE TOTAL HOURS: ");
+				hr = input.nextDouble();
+
+				System.out.println("============ CHARGES ===============");
+				System.out.println("VEHICLE TYPE: CAR");
+				if (hr > 0.5) {
+					double amount = (hr - 0.5) * 2.30 + 2.00;
+					System.out.println("CHARGES = " + df.format(amount));
+				} else {
+					double amount = (hr) * 1.00;
+					System.out.println("CHARGES= " + df.format(amount));
+				}
+				break;
+			case 3:
+				System.out.print("ENTER THE TOTAL HOURS: ");
+				hr = input.nextDouble();
+
+				System.out.println("============ CHARGES ===============");
+				System.out.println("VEHICLE TYPE: TRUCK");
+				if (hr > 1) {
+					double amount = (hr - 1) * 3.70 + 2.00;
+					System.out.println("CHARGES = " + df.format(amount));
+				} else {
+					double amount = (hr) * 2.00;
+					System.out.println("CHARGES = " + df.format(amount));
+				}
+				break;
+			case 4:
+				quit = true;
+				break;
+			default:
+				System.out.println("INVALID ENTRY!");
+			}
+		}
+
+		while (!quit);
+	}
+}
